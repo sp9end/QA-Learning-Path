1ae59d1be9303ee5cbd58ac91372116b0324089b
sp9end <sp9end@gmail.com>
2026-04-08 14:43:06 +0200

    Add first Python script and test cases

---
+++ Module-02-Python-Basics/hello_qa.py
@@ -0,0 +1,8 @@
+# To jest Twój pierwszy skrypt
+name = "Junior QA"
+print(f"Witaj w świecie automatyzacji, {name}!")
+
+# Prosta asercja (sprawdzenie)
+expected_value = "Junior QA"
+assert name == expected_value
+print("Asercja przeszła pomyślnie!")
